8ed612fc03a219e78fe986a75eb94fcb23db0b15
Valentyn Mostovyi <mostovoy.v@gmail.com>
2025-04-29 16:32:40 +0200

    Server component includes Client component

---
+++ src/app/components/server-component-copy.tsx
@@ -0,0 +1,21 @@
+import { headers } from 'next/headers';
+import React from 'react';
+
+export interface ServerComponentCopyProps {
+  children?: React.ReactNode;
+}
+
+export default function ServerComponentCopy({
+  children,
+}: ServerComponentCopyProps) {
+  // Серверна фукція (щоб компонент 100% був серверним). Якщо Серверний компонент не містить серверних функцій, то він стає Клєнтським
+  console.log(headers().get('user-agent'));
+
+  console.log('Server Component Copy');
+  return (
+    <div>
+      <span>Server Component Copy</span>
+      {children}
+    </div>
+  );
+}
